f6ecbd1ba2616446a60e03d0d48e06be77cfe651
manthana76 <chandrucnu451@gmail.com>
2026-09-11 14:36:49 +0530

    Correct age input prompt and eligibility checks
    Fixed syntax errors and improved input prompt.

--- vote.py
+++ vote.py
@@ -1,6 +1,8 @@
-age = int(input("Enter your age:"))
+age = int(input("Enter your age"))
 
 if age >=18;
    print("you are eligible to vote.")
+   print("you can register as a voter.")
 else:
    print("you are not eligible to vote.")
+   print("you must be aleast 18 years old.")
